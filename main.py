58ae14ec12e45f32de5d21bd07c1d83c90e09c8a
Geet Raju <geet.raju1@gmail.com>
2023-02-17 10:11:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import streamlit as st
+
+# The code below is for the title and logo for this page.
+st.set_page_config(page_title="EmpAct Cooperative App (beta)", page_icon="📊")
+
+
+st.title("`Preface`")
+
+st.write("")
+
+st.markdown("""
+
+    This report is developed using [Streamlit](https://streamlit.io/) and is meant for demonstration purposes only.
+    
+"""
+)
+
+st.write("Scope")
+
+st.markdown("""
+
+   The scope of the audit included two high priority potential talent management dimension contained within Deloitte's Talent Maturity Model:
+   
+   - `Talent strategy`
+   - `Recruitment and Staffing Strategies`
+   
+    The scope of this audit covered the timeframe October 1, 2022 to December 31, 2022.
+    
+    
+""")
+
+st.write("Approch")
+
+st.markdown("""
+
+    Procedures for gathering evidence included a gathering feedback through a questionnaire, 
+    computation and analysis of data. 
+
+    The questionnaire can be found [here](https://2k9v7hvpbj1.typeform.com/to/iBAEzh3n).
+
+""")
+
+st.write("")
+
