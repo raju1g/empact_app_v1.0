3a7586880370696ff2eb9306cf034bbf37894026
Geet Raju <geet.raju1@gmail.com>
2023-02-21 22:09:50 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,6 +7,9 @@ from dataclasses import dataclass
 from typing import List
 import random
 from lifelines import KaplanMeierFitter
+import bqplot.pyplot as plt
+import ipywidgets as widgets
+
 
 ## Data Visualization:
 import matplotlib.pyplot as plt
@@ -26,20 +29,19 @@ from scipy.stats.stats import pearsonr
 ## Let's import lifelines
 import lifelines
 
-# The code below is for the title and logo for this page.
-st.set_page_config(page_title="EmpAct Cooperative App (beta)", page_icon="📊")
-
-st.title("Talent management audit (2022-23) - XYZ Oy")
-
-st.subheader("Preface")
+st.set_page_config(page_title="EmpAct Cooperative App",
+                   initial_sidebar_state="collapsed",
+                   page_icon="🔮")
 
 
-@dataclass(frozen=True)
-class Variant:
-    gender: str
-    typ: str
-    reason: str
-    benefits: str
+tabs = ["Main", "About"]
+page = st.sidebar.radio("Tabs", tabs)
+
+
+@st.cache(persist=False,
+          allow_output_mutation=True,
+          suppress_st_warning=True,
+          show_spinner=True)
 
 
 @st.experimental_memo
@@ -74,127 +76,18 @@ def load_data():
     return df
 
 
-df = load_data()
+if tabs == 'Main':
+    def create_scatter(feature1, feature2):
+        fig = plt.figure(title="%s vs %s Relation" % (feature1.capitalize(), feature2.capitalize()))
 
-# Initialization
-if "type_use_input" not in st.session_state:
-    st.session_state.type_use_input = df["type"].unique()
+        scat = plt.scatter(x=df[feature1],
+                           y=df[feature2],
+                           color=df["age"],
+                           )
 
-if "reg_use_input" not in st.session_state:
-    st.session_state.reg_use_input = df["reason"].unique()
+        plt.xlabel(feature1.capitalize())
+        plt.ylabel(feature2.capitalize())
 
-if "gen_use_input" not in st.session_state:
-    st.session_state.gen_use_input = df["gender"].unique()
+        plt.show()
 
-if "ben_use_input" not in st.session_state:
-    st.session_state.ben_use_input = df["benefits"].unique()
 
-row00_0, row00_1, row00_2 = st.columns([0.5, 0.25, 1])
-with row00_0:
-    container = st.container()
-    all = st.checkbox("All", value=True)
-    if all:
-        typ = container.multiselect(
-            "Type:",
-            st.session_state.type_use_input,
-            default=st.session_state.type_use_input,
-        )
-        gender = container.multiselect(
-            "Gender:",
-            st.session_state.gen_use_input,
-            default=st.session_state.gen_use_input,
-        )
-        reason = container.multiselect(
-            "Reason:",
-            st.session_state.reg_use_input,
-            default=st.session_state.reg_use_input,
-        )
-        benefits = container.multiselect(
-            "Benefits:",
-            st.session_state.ben_use_input,
-            default=st.session_state.ben_use_input,
-        )
-    else:
-        typ = container.multiselect(
-            "Type:",
-            st.session_state.type_use_input,
-        )
-        gender = container.multiselect(
-            "Gender:",
-            st.session_state.gen_use_input,
-        )
-        reason = container.multiselect(
-            "Reason:",
-            st.session_state.reg_use_input,
-        )
-        benefits = container.multiselect(
-            "Benefits:",
-            st.session_state.ben_use_input,
-        )
-
-sel = [gender, typ, reason, benefits]
-variants = list(itertools.product(*sel))
-variants: List[Variant] = [
-    Variant(gender=i[0], reason=i[1], typ=i[2], benefits=i[3])
-    for i in variants
-]
-totals = {}
-
-for df in df.items():
-    filtered_dfs = []
-
-    for variant in variants:
-        filtered_df = df[
-            (df["gender"] == variant.gender)
-            (df["reason"] == variant.reason)
-            & (df["type"] == variant.typ)
-            & (df["benefits"] == variant.benefits)
-            ]
-
-        if not filtered_df.empty:
-            filtered_dfs.append(filtered_df)
-
-try:
-    kmf = KaplanMeierFitter()
-    fig, ax = plt.subplots(figsize=(10, 5), dpi=500)
-
-    ## Employees with coaching
-
-    cohort1 = filtered_dfs[filtered_dfs["type"] == "voluntary"]
-
-    kmf.fit(durations=cohort1["years_tenure"],
-            event_observed=cohort1["event"],
-            label='Voluntary turnover')
-
-    kmf.plot_survival_function(ax=ax, ci_show=False)
-
-    ## Employees without coaching
-
-    cohort2 = filtered_dfs[filtered_dfs["type"] != "voluntary"]
-
-    kmf.fit(durations=cohort2["years_tenure"],
-            event_observed=cohort2["event"],
-            label='Involuntary turnover')
-
-    ## Adding a few details to the plot
-
-    kmf.plot_survival_function(ax=ax, ci_show=False)
-
-    ax.set_ylabel("Employee survival rate")
-    ax.set_xlabel("Timeline - years")
-
-    plt.text(5.15, 0.7, '50% voluntary turnover', size=10, color='lightblue')
-    plt.text(5.15, 0.66, 'after 5 years', size=10, color='lightblue')
-    plt.text(-0.5, 0.3, '50% involuntary turnover', size=10, color='orange')
-    plt.text(-0.5, 0.26, 'after 3.5 years', size=10, color='orange')
-    plt.axvline(x=4.9, color='lightblue', linestyle='--')
-    plt.axvline(x=3.5, color='orange', linestyle='--')
-
-    plt.legend(fontsize=9)
-
-    for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
-                 ax.get_xticklabels() + ax.get_yticklabels()):
-        item.set_fontsize(10)
-    fig
-except IndexError:
-    st.warning("This is throwing an exception, bear with us!")
