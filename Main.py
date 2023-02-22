e0e4f583b13147bf36d4035662790822f42677f7
Geet Raju <geet.raju1@gmail.com>
2023-02-22 09:52:07 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -63,132 +63,137 @@ def load_data():
     df = df.drop(["stag"], axis=1)
     df['years_tenure'] = df["years_tenure"].round(1)
     return df
-  
-utils.local_css("style_trial.css")   
-st.write(
-f"""
-<div class="base-wrapper" style="background-color:#00A300;">
-    <div class="hero-wrapper">
-        <div class="hero-container" style="width:100%; height:100px">
-            <div class="hero-container-content">
-                <span class="subpages-subcontainer-product white-span" style="margin-left: -0.5em;">Talent management audit</span>
+
+
+def main():
+    utils.local_css("style_trial.css")   
+    st.write(
+    f"""
+    <div class="base-wrapper" style="background-color:#00A300;">
+        <div class="hero-wrapper">
+            <div class="hero-container" style="width:100%; height:100px">
+                <div class="hero-container-content">
+                    <span class="subpages-subcontainer-product white-span" style="margin-left: -0.5em;">Talent management audit</span>
+                </div>
             </div>
         </div>
     </div>
-</div>
-""",
-unsafe_allow_html=True,
-)
+    """,
+    unsafe_allow_html=True,
+    )
 
-st.write(
-f"""
-<br><br><br>
-""", unsafe_allow_html=True,
-)
+    st.write(
+    f"""
+    <br><br><br>
+    """, unsafe_allow_html=True,
+    )
 
-df = load_data()
-with st.expander("Show the `employee turnover` dataframe"):
-    st.dataframe(df, 100, 200)
+    df = load_data()
+    with st.expander("Show the `employee turnover` dataframe"):
+        st.dataframe(df, 100, 200)
 
-st.write(
-f"""
-<br><br><br>
-""", unsafe_allow_html=True,
-)
+    st.write(
+    f"""
+    <br><br><br>
+    """, unsafe_allow_html=True,
+    )
 
-gen_to = df['gender'].unique().tolist() 
-reg_to = df['reason'].unique().tolist() 
-typ_to = df['type'].unique().tolist() 
-ben_to = df['benefits'].unique().tolist() 
+    gen_to = df['gender'].unique().tolist() 
+    reg_to = df['reason'].unique().tolist() 
+    typ_to = df['type'].unique().tolist() 
+    ben_to = df['benefits'].unique().tolist() 
 
-# Create a table to have filters side by side :
-col1, col2 = st.columns([0.5,0.5])
-with col1:
-    selected_gento = st.multiselect(
-        'Gender:',
-        gen_to
-        )
-with col2:
-    selected_typto = st.multiselect(
-        'Turnover type:',
-        typ_to
-        )
-st.write(
-f"""
-<br>
-""", unsafe_allow_html=True,
-)
-col3, col4 = st.columns([0.5,0.5])
-with col3:
-    selected_regto = st.multiselect(
-        'Turnover reason:',
-        reg_to
-        )
-with col4:
-    selected_bento = st.multiselect(
-        'Benefits:',
-        ben_to
-        )
-df_filtered = df[
-    df['gender'].isin(selected_gento) & 
-    df['type'].isin(selected_typto) &
-    df['reason'].isin(selected_regto) &
-    df['benefits'].isin(selected_bento)
-    ]
-# Avoid empty dataframe when no filters selected : 
-def filtered_data(df):
-    if df_filtered.empty :
-       st.write(df)
-    else : st.write(df_filtered)
-    return df_filtered
+    # Create a table to have filters side by side :
+    col1, col2 = st.columns([0.5,0.5])
+    with col1:
+        selected_gento = st.multiselect(
+            'Gender:',
+            gen_to
+            )
+    with col2:
+        selected_typto = st.multiselect(
+            'Turnover type:',
+            typ_to
+            )
+    st.write(
+    f"""
+    <br>
+    """, unsafe_allow_html=True,
+    )
+    col3, col4 = st.columns([0.5,0.5])
+    with col3:
+        selected_regto = st.multiselect(
+            'Turnover reason:',
+            reg_to
+            )
+    with col4:
+        selected_bento = st.multiselect(
+            'Benefits:',
+            ben_to
+            )
+    df_filtered = df[
+        df['gender'].isin(selected_gento) & 
+        df['type'].isin(selected_typto) &
+        df['reason'].isin(selected_regto) &
+        df['benefits'].isin(selected_bento)
+        ]
+    # Avoid empty dataframe when no filters selected : 
+    def filtered_data(df):
+        if df_filtered.empty :
+           st.write(df)
+        else : st.write(df_filtered)
+        return df_filtered
 
-# Write data frame :
-st.write(filtered_data)
-# try:
-kmf = KaplanMeierFitter()
-fig, ax = plt.subplots(figsize=(10, 5), dpi=500)
+    # Write data frame :
+    st.write(filtered_data)
+    # try:
+    kmf = KaplanMeierFitter()
+    fig, ax = plt.subplots(figsize=(10, 5), dpi=500)
 
-## Employees with coaching
+    ## Employees with coaching
 
-cohort1 = df[df['type'] == "voluntary"]
+    cohort1 = df[df['type'] == "voluntary"]
 
-kmf.fit(durations=cohort1["years_tenure"],
-        event_observed=cohort1["event"],
-        label='Voluntary turnover')
+    kmf.fit(durations=cohort1["years_tenure"],
+            event_observed=cohort1["event"],
+            label='Voluntary turnover')
 
-kmf.plot_survival_function(ax=ax, ci_show=False)
+    kmf.plot_survival_function(ax=ax, ci_show=False)
 
-## Employees without coaching
+    ## Employees without coaching
 
-cohort2 = df[df['type'] != "voluntary"]
+    cohort2 = df[df['type'] != "voluntary"]
 
-kmf.fit(durations=cohort2["years_tenure"],
-        event_observed=cohort2["event"],
-        label='Involuntary turnover')
+    kmf.fit(durations=cohort2["years_tenure"],
+            event_observed=cohort2["event"],
+            label='Involuntary turnover')
 
-## Adding a few details to the plot
+    ## Adding a few details to the plot
 
-kmf.plot_survival_function(ax=ax, ci_show=False)
+    kmf.plot_survival_function(ax=ax, ci_show=False)
 
-ax.set_ylabel("Employee survival rate")
-ax.set_xlabel("Timeline - years")
+    ax.set_ylabel("Employee survival rate")
+    ax.set_xlabel("Timeline - years")
 
-plt.text(5.15, 0.7, '50% voluntary turnover', size=10, color='lightblue')
-plt.text(5.15, 0.66, 'after 5 years', size=10, color='lightblue')
-plt.text(-0.5, 0.3, '50% involuntary turnover', size=10, color='orange')
-plt.text(-0.5, 0.26, 'after 3.5 years', size=10, color='orange')
-plt.axvline(x=4.9, color='lightblue', linestyle='--')
-plt.axvline(x=3.5, color='orange', linestyle='--')
+    plt.text(5.15, 0.7, '50% voluntary turnover', size=10, color='lightblue')
+    plt.text(5.15, 0.66, 'after 5 years', size=10, color='lightblue')
+    plt.text(-0.5, 0.3, '50% involuntary turnover', size=10, color='orange')
+    plt.text(-0.5, 0.26, 'after 3.5 years', size=10, color='orange')
+    plt.axvline(x=4.9, color='lightblue', linestyle='--')
+    plt.axvline(x=3.5, color='orange', linestyle='--')
 
-plt.legend(fontsize=9)
+    plt.legend(fontsize=9)
 
-for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
-             ax.get_xticklabels() + ax.get_yticklabels()):
-    item.set_fontsize(10)
-fig
-# except IndexError:
-#    st.warning("This is throwing an exception, bear with us!")
+    for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                 ax.get_xticklabels() + ax.get_yticklabels()):
+        item.set_fontsize(10)
+    fig
+    # except IndexError:
+    #    st.warning("This is throwing an exception, bear with us!")
 
+
+if __name__ == '__main__':
+    main()
 
 
 
